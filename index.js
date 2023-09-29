e42278c3d0018d50b9bb781e4351eb04818fc6ac
Din <Dinz5005@gmail.com>
2023-09-29 09:54:45 +0300

    index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const customer = require("./routes/customer");
 const employee = require("./routes/employee");
 
 app.use(express.json());
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: '*' }));
 app.use("", user);
 app.use("/customers", customer);
 app.use("/employees", employee);
